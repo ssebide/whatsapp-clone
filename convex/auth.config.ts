69d206cd5271f3dbb6ac21ceed04b746bf39dd4a
Infinitylabs420 <154331573+Infinitylabs420@users.noreply.github.com>
2024-07-21 11:24:09 +0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://suitable-trout-44.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "colorless-goose-369.convex.cloud" },
+			{ hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+		],
+	},
+};
 
 export default nextConfig;
